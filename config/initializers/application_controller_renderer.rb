04d33c20d69bc8f0f5088e068592fc0543361f31
1994reka3 <rik3@re1kas-MacBookAir.local>
2020-07-30 21:13:03 +0900

    [Finish]5-3投稿画像詳細ページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'df8ac1a2b099db7050a20a9c0c9446fc8941547398243fa7a0bd6d767e0c993e51c3aeff3c8cc12c8e9fb11927ca6222a1ea348f7759aac5988708fa9db92d9d'
